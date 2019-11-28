f66c0b41198706acbee2d77e3cfe479ceb0e8957
anil_m <anil_mndl@yahoo.com>
2019-11-28 13:18:47 +0545

    first attempt with splunk logger

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -53,12 +57,14 @@ public class UserController {
 
 		if(createUserRequest.getPassword().length() < 7 &&
 				createUserRequest.getPassword() != createUserRequest.getConfirmPassword()){
-			System.out.println("Error with user password. Cannot create user " + createUserRequest.getUsername());
+			logger.info("Error with user password. Cannot create user " + createUserRequest.getUsername());
 		}
 
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 
 		userRepository.save(user);
+
+		logger.info("User with username " + user.getUsername() + " successfully logged in.");
 		return ResponseEntity.ok(user);
 	}
 	
